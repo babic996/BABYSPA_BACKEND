f989e164e0d64863583ddbffec8d8ae22bd05a85
babic996 <aleksandar.babic996@gmail.com>
2025-09-17 00:28:39 +0200

    Update DailyReportScheduler.java

--- src/main/java/com/backend/babyspa/v1/scheduler/DailyReportScheduler.java
+++ src/main/java/com/backend/babyspa/v1/scheduler/DailyReportScheduler.java
@@ -1,44 +1,39 @@
 package com.backend.babyspa.v1.scheduler;
 
 import java.util.Arrays;
-import java.util.List;
 
-import com.backend.babyspa.v1.models.TenantNames;
-import com.backend.babyspa.v1.services.BabyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.backend.babyspa.v1.models.TenantNames;
+import com.backend.babyspa.v1.services.BabyService;
 import com.backend.babyspa.v1.services.ReservationService;
 
 @Component
 public class DailyReportScheduler {
 
-    @Autowired
-    ReservationService reservationService;
+	@Autowired
+	ReservationService reservationService;
 
-    @Autowired
-    BabyService babyService;
+	@Autowired
+	BabyService babyService;
 
-    @Value("${tenants}")
-    private String tenantsProperty;
+	@Scheduled(cron = "00 59 23 * * *", zone = "Europe/Sarajevo")
+	public void generateReports() {
+		Arrays.stream(TenantNames.values())
+				.forEach(tenant -> reservationService.generateReportForAllDateInReservation(true, null, tenant.name()));
+	}
 
-    @Scheduled(cron = "00 59 23 * * *", zone = "Europe/Sarajevo")
-    public void generateReports() {
-        Arrays.stream(TenantNames.values())
-                .forEach(tenant -> reservationService.generateReportForAllDateInReservation(true, null, tenant.name()));
-    }
+	@Scheduled(cron = "0 0 1 * * *", zone = "Europe/Sarajevo")
+	public void updateReservationStatusDayBefore() {
+		reservationService.updateReservationWithStatusCreatedToStatusUsed();
+	}
 
-    @Scheduled(cron = "0 0 1 * * *", zone = "Europe/Sarajevo")
-    public void updateReservationStatusDayBefore() {
-        reservationService.updateReservationWithStatusCreatedToStatusUsed();
-    }
+	@Scheduled(cron = "0 59 23 * * *", zone = "Europe/Sarajevo")
+	public void updateAllNumberOfMonths() {
 
-    @Scheduled(cron = "0 59 23 * * *", zone = "Europe/Sarajevo")
-    public void updateAllNumberOfMonths() {
-
-        babyService.updateMonthsForAll();
-    }
+		babyService.updateMonthsForAll();
+	}
 
 }
